34647ac75ed043f84af62ad0de1599b858438e36
Captain2409 <88193206+Captain2409@users.noreply.github.com>
2021-11-25 13:10:33 +0100

    Add files via upload

---
+++ Template Website/js/main.js
@@ -0,0 +1,9 @@
+console.log("Kilo Development");
+
+document.getElementById("menu").addEventListener("click", openMenu,);
+
+function openMenu(){
+
+    document.getElementById("Dorpdown").classList.toggle("active");
+}
+console.log ("Wer das liest is schwul")
