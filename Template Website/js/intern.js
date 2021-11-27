162e41be978be2f52aed6627c797225cd6437bc2
Captain2409 <88193206+Captain2409@users.noreply.github.com>
2021-11-27 11:16:58 +0100

    Add files via upload

---
+++ Template Website/js/intern.js
@@ -0,0 +1,13 @@
+var Käse ="Null"
+
+function logger(){
+
+    var Käse1 = (document.getElementById("pwd").addEventListener(val))
+}
+
+if (Käse == Käse1){
+   console.log("Login Succesfully")
+
+   document.getElementById("Dorpdown").classList.toggle("active");
+}
+
